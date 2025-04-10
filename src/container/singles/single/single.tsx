9836ec52334fcc14a6ff4cb5d62752a8e0b427f9
SkyMods123 <59374835+SkyMods123@users.noreply.github.com>
2025-04-10 20:53:04 +0200

    Update single.tsx

--- src/container/singles/single/single.tsx
+++ src/container/singles/single/single.tsx
@@ -39,7 +39,7 @@ const SingleType1: FC<SingleType1Props> = ({ post, showRightSidebar }) => {
       skip: !databaseId
     });
 
-    const relatedPosts = relatedPostsData?.posts?.nodes || [];
+    const relatedPosts = (relatedPostsData?.posts?.nodes || []).slice(0, 4);
 
     // Hook za meta podatke
     const { loading: loadingRelatedMeta } = useGetPostsNcmazMetaByIds({
