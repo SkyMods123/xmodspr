39c4b6b57a71ee9f00e2ec22ac89fdef79dfd64a
SkyMods123 <59374835+SkyMods123@users.noreply.github.com>
2025-04-10 22:05:42 +0200

    Update single.tsx

--- src/container/singles/single/single.tsx
+++ src/container/singles/single/single.tsx
@@ -64,7 +64,7 @@ const SingleType1: FC<SingleType1Props> = ({ post, showRightSidebar }) => {
                                 <header className="!mt-0">
                                     <div className="rounded-lg border text-card-foreground shadow-sm bg-card/70 backdrop-blur-sm">
                                         <div className="flex flex-col space-y-1.5 p-6 pb-4">
-                                            <div className="flex flex-col gap-1">
+                                            <div className="flex flex-col gap-1 -bottom-[3px]">
 						    <CategoryBadgeList
 								itemClass="!px-3"
 								categories={categories?.nodes || []}
