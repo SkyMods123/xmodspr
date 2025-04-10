9a06131eba6c1a0673338e759a990cfcb00a7570
SkyMods123 <59374835+SkyMods123@users.noreply.github.com>
2025-04-10 18:33:25 +0200

    Update single.tsx

--- src/container/singles/single/single.tsx
+++ src/container/singles/single/single.tsx
@@ -13,9 +13,7 @@ import SingleRelatedPosts from '@/container/singles/SingleRelatedPosts';
 import { GET_RELATED_POSTS } from '@/container/singles/single/related';
 import PostCardLikeAndComment from '@/components/PostCardLikeAndComment/PostCardLikeAndComment'
 import NcBookmark from '@/components/NcBookmark/NcBookmark'
-import PostCardLikeAction, {
-	PostCardLikeActionProps,
-} from '@/components/PostCardLikeAction/PostCardLikeAction'
+import PostCardLikeAction from '@/components/PostCardLikeAction/PostCardLikeAction'
 
 export interface SingleType1Props {
     post: FragmentTypePostFullFields;
@@ -162,9 +160,10 @@ const SingleType1: FC<SingleType1Props> = ({ post, showRightSidebar }) => {
 								containerClassName="inline-flex items-center justify-center gap-2 whitespace-nowrap text-sm font-medium ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 text-primary-foreground h-10 px-4 bg-neutral-50 hover:bg-neutral-100 dark:bg-neutral-800 dark:hover:bg-neutral-700 dark:text-neutral-200 border border-input"
 							/>
 							    <PostCardLikeAction
-								likeCount={likeCount}
-								postDatabseId={postDatabseId}
+								likeCount={ncPostMetaData?.likesCount || 0}
+								postDatabseId={databaseId}
 							/>
+							<div className="h-4 border-s border-neutral-200 dark:border-neutral-700"></div>
                                                         <button
                                                             className="inline-flex items-center transition-colors hover:bg-neutral-50 dark:hover:bg-neutral-800 justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 border border-input bg-background hover:text-accent-foreground h-10 px-4 py-2 col-span-2 transition-colors duration-200 hover:bg-accent"
                                                         >
