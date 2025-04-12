85df93d4dbef799096aea36b97fb89cd00986539
SkyMods123 <59374835+SkyMods123@users.noreply.github.com>
2025-04-12 18:52:29 +0200

    Update single.tsx

--- src/container/singles/single/single.tsx
+++ src/container/singles/single/single.tsx
@@ -224,7 +224,7 @@ const SingleType1: FC<SingleType1Props> = ({ post, showRightSidebar }) => {
                                     <div className="rounded-lg border bg-card text-card-foreground shadow-sm">
                                         <div className="flex flex-col space-y-1.5 p-6">
                                             <div className="text-2xl font-semibold leading-none tracking-tight">
-                                                <h2>Comments >{commentCount || 0}</h2>
+                                                <h2 className="description prose prose-invert max-w-none text-neutral-900 dark:text-neutral-100">Comments ({commentCount})</h2> 
                                             </div>
                                         </div>
                                         <div className="p-6 pt-0 space-y-4">
