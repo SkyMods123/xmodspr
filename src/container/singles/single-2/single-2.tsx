5082036db75b2fcd74f1a35380fc9b0f22e1ad42
SkyMods123 <59374835+SkyMods123@users.noreply.github.com>
2025-04-12 20:24:13 +0200

    Update single-2.tsx

--- src/container/singles/single-2/single-2.tsx
+++ src/container/singles/single-2/single-2.tsx
@@ -7,6 +7,7 @@ import { SingleType1Props } from "../single/single";
 import { GET_RELATED_POSTS } from '@/container/singles/single/related';
 interface Props extends SingleType1Props {}
 import SingleRelatedPosts2 from '@/container/singles/SingleRelatedPosts2';
+import { gql, useQuery } from '@apollo/client';
 
 const SingleType2: FC<Props> = ({ post }) => {
   //
